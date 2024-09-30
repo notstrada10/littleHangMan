b71c48a252d649042a38890ec84a1ff923a80386
Marco Strada <marcostrada10@gmail.com>
2024-09-30 18:20:53 +0200

    Created GuessWord div

--- src/HangManWord.tsx
+++ src/HangManWord.tsx
@@ -1,15 +1,15 @@
+const word = "hello";
+const guessedLetters = ["h", "l"];
 
 export function HangManWord() {
-
     return (
-  
-      <div>
-  
-        {/* Your JSX content here */}
-  
-      </div>
-  
-    );
-  
-  }
+        <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"}}>
+            {word.split("").map((letter, index) => (
+                <span style={{ borderBottom: ".1em solid black" }} key={index}>
+                    <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden", }}>{letter}</span>
+                </span>
+            ))}
+        </div>
+    )
+}
   
